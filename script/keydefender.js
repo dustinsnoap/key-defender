83bbb87e310e3f852859b49d479a222f218dbb31
dustinsnoap <afrobacon@gmail.com>
2019-01-09 19:28:38 -0500

    Added code to create canvases for game Added CSS to have canvases overlap Added 3 canvas: background, keyfender, overlay

--- script/keydefender.js
+++ script/keydefender.js
@@ -1,1 +1,14 @@
-// EMPTY
+window.onload = function() {
+    addCanvases();
+}
+function addCanvases() {
+    createCanvas("background", "game");
+    createCanvas("keyfender", "game");
+    createCanvas("overlay", "game");
+}
+function createCanvas(name, parentID) {
+    let parent = document.getElementById(parentID);
+    let canvas = document.createElement("canvas");
+    canvas.classList.add(name);
+    parent.appendChild(canvas);
+}
